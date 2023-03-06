5e94e5f6d334a5e28b19c31add988dfc424939e9
OlympioMatheus <94844577+OlympioMatheus@users.noreply.github.com>
2023-03-06 15:52:45 -0300

    Add files via upload

---
+++ Python/Converter hora para minutos.py
@@ -0,0 +1,4 @@
+import math
+tempo = int(input('digite o tempo em horas:'))
+tempo_m = int(tempo * 60)
+print (f'transformando horas em minutos, temos {tempo_m} minutos')
---
+++ Python/Converter hora para segundos.py
@@ -0,0 +1,4 @@
+import math
+tempo = int(input('digite o tempo em horas:'))
+tempo_s = int(tempo * 3600)
+print (f'transformando horas em segundos, temos {tempo_s} segundos')
